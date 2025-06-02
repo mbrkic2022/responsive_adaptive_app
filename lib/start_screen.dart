7fef7137f83aad0125afc73546c3ca8133602bb3
Marija Brkić Bakarić <mbrkic@uniri.hr>
2025-06-02 19:53:15 +0200

    Add platform-specific widgets

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_app/gradient_container.dart';
 
@@ -42,8 +45,33 @@ class _StartScreenState extends State<StartScreen> {
   //               text: _textEditingController.text)));
   // }
 
-    void onPressed() {
+  void onPressed() {
     if (_textEditingController.text.trim().isEmpty) {
+      // showDialog(
+      //     context: context,
+      //     builder: (ctx) => AlertDialog(
+      //             title: const Text('Invalid input'),
+      //             content: const Text('Make sure the text field is not empty!'),
+      //             actions: [
+      //               TextButton(
+      //                 onPressed: () => Navigator.pop(ctx),
+      //                 child: const Text('Ok'),
+      //               )
+      //             ]));
+      _showDialog();
+    } else {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (ctx) => GradientContainer(
+                  color1: Colors.purple,
+                  color2: Colors.yellow,
+                  text: _textEditingController.text)));
+    }
+  }
+
+  void _showDialog() {
+    if (Platform.isAndroid) {
       showDialog(
           context: context,
           builder: (ctx) => AlertDialog(
@@ -56,15 +84,17 @@ class _StartScreenState extends State<StartScreen> {
                     )
                   ]));
     } else {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (ctx) => GradientContainer(
-                  color1: Colors.purple,
-                  color2: Colors.yellow,
-                  text: _textEditingController.text)));
+      showCupertinoDialog(
+          context: context,
+          builder: (ctx) => CupertinoAlertDialog(
+                  title: const Text('Invalid input'),
+                  content: const Text('Make sure the text field is not empty!'),
+                  actions: [
+                    CupertinoDialogAction(
+                      onPressed: () => Navigator.pop(ctx),
+                      child: const Text('Ok'),
+                    )
+                  ]));
     }
   }
-}
-
 }
