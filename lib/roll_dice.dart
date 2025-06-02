b0061785a4d4d743544bd2398b18b917630dae32
Marija Brkić Bakarić <mbrkic@uniri.hr>
2025-06-02 19:48:46 +0200

    Build adaptive UI

--- lib/roll_dice.dart
+++ lib/roll_dice.dart
@@ -23,26 +23,65 @@ class _RollDiceState extends State<RollDice> {
 
   @override
   Widget build(BuildContext context) {
-    if (MediaQuery.of(context).orientation == Orientation.landscape) {
-      return const Center(
-        child: Text('Please rotate your device to portrait mode'),
-      );
+    // if (MediaQuery.of(context).orientation == Orientation.landscape) {
+    //   return const Center(
+    //     child: Text('Please rotate your device to portrait mode'),
+    //   );
+    // } else {
+    //   return Center(
+    //     child: Column(
+    //       mainAxisSize: MainAxisSize.min,
+    //       children: [
+    //         Text(widget.text),
+    //         Image.asset('assets/images/dice-$dice.png', width: 200),
+    //         TextButton(
+    //           onPressed: onPressed,
+    //           child: const Text('Roll dice'),
+    //         ),
+    //         TextButton(onPressed: onPressed1, child: const Text('Go back'))
+    //       ],
+    //     ),
+    //   );
+    // }
+    if (MediaQuery.of(context).size.width < 600) {
+      return _buildPortraitMode();
     } else {
-      return Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(widget.text),
-            Image.asset('assets/images/dice-$dice.png', width: 200),
-            TextButton(
-              onPressed: onPressed,
-              child: const Text('Roll dice'),
-            ),
-            TextButton(onPressed: onPressed1, child: const Text('Go back'))
-          ],
-        ),
-      );
+      return _buildLandscapeMode();
     }
+  }
+
+  Widget _buildPortraitMode() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(widget.text),
+          Image.asset('assets/images/dice-$dice.png', width: 200),
+          TextButton(
+            onPressed: onPressed,
+            child: const Text('Roll dice'),
+          ),
+          TextButton(onPressed: onPressed1, child: const Text('Go back'))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLandscapeMode() {
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(widget.text),
+          Image.asset('assets/images/dice-$dice.png', width: 200),
+          TextButton(
+            onPressed: onPressed,
+            child: const Text('Roll dice'),
+          ),
+          TextButton(onPressed: onPressed1, child: const Text('Go back'))
+        ],
+      ),
+    );
   }
 
   void onPressed1() {
