bfe760d71bbefc2143cf4bad67d20da814a089ac
Marija Brkić Bakarić <mbrkic@uniri.hr>
2025-06-02 19:54:49 +0200

    Add color scheme and modify theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/start_screen.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(seedColor: Colors.purple);
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primary,
+          foregroundColor: kColorScheme.onPrimary,
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onPrimaryContainer,
+              ),
+            ),
+      ),
       home: StartScreen(),
     ),
   );
